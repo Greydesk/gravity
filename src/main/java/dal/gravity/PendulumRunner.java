183c645dd1b46dd66cdd6fbe6a6df6b3ef21995a
Greydesk <rod.naugler@gmail.com>
2017-04-04 12:14:42 -0300

    Refactoring - Collapse Hierarchy

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -14,10 +14,12 @@ public class PendulumRunner {
 
 	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
 	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
-	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta);
-	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0);
+    double Gravity = 9.80665;
+
+	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta, Gravity);
+	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, Gravity);
 	RegularPendulum rpCoarse = 
-	    new RegularPendulum (sLen, pMass, theta0, .1);
+	    new RegularPendulum (sLen, pMass, theta0, .1, Gravity);
 
 	// print out difference in displacement in 1 second intervals
 	// for 20 seconds
