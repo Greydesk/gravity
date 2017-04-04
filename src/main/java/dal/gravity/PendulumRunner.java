6172aafcab7965d63f459949c39953ba722c0375
Greydesk <rod.naugler@gmail.com>
2017-04-04 13:04:39 -0300

    Refactoring Step 3

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -9,33 +9,49 @@ import java.text.NumberFormat;
 public class PendulumRunner {
 
     public static void main (String [] args) {
-	NumberFormat nf = NumberFormat.getInstance ();
-	nf.setMaximumFractionDigits (3);
+    	NumberFormat nf = NumberFormat.getInstance ();
+    	nf.setMaximumFractionDigits (3);
 
-	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
-	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
-//    double Gravity = 9.80665;
-    GravityConstant Gravity = new GravityConstant(9.80665);
+    	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
+    	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
+    	GravityConstant Gravity = new GravityConstant(9.80665);
 
-	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta, Gravity);
-	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, Gravity);
-	RegularPendulum rpCoarse = 
-	    new RegularPendulum (sLen, pMass, theta0, .1, Gravity);
+    	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta, Gravity);
+    	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, Gravity);
+    	RegularPendulum rpCoarse = new RegularPendulum (sLen, pMass, theta0, .1, Gravity);
 
-	// print out difference in displacement in 1 second intervals
-	// for 20 seconds
-	int iterations = (int) (1/delta);
-	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
-	for (int second = 1; second <= 20; second++) {
-	    for (int i = 0; i < iterations; i++) rp.step ();
-	    for (int i = 0; i < 10; i++) rpCoarse.step (); 
-	    System.out.println ("t=" + second + "s: \t" + 
+    	// print out difference in displacement in 1 second intervals for 10 seconds
+    	int iterations = (int) (1/delta);
+    	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
+    	for (int second = 1; second <= 10; second++) {
+    		for (int i = 0; i < iterations; i++) rp.step ();
+    		for (int i = 0; i < 10; i++) rpCoarse.step (); 
+    		System.out.println ("t=" + second + "s: \t" + 
 				nf.format (Math.toDegrees (sp.getTheta (second))) 
 				+ "\t" +
 				nf.format (Math.toDegrees (rp.getLastTheta ()))
 				+ "\t" + 
 				nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
-	}
+    	}
+    	iterations = (int) (1/delta);
+    	System.out.println("Change Gravity to 25m/s^2");
+    	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
+    	Gravity = new GravityConstant(25);
+    	rp = new RegularPendulum (sLen, pMass, theta0, delta, Gravity);
+    	sp = new SimplePendulum (sLen, pMass, theta0, Gravity);
+    	rpCoarse = new RegularPendulum (sLen, pMass, theta0, .1, Gravity);
+    	
+    	for (int second = 11; second <= 20; second++) {
+    		for (int i = 0; i < iterations; i++) rp.step ();
+    		for (int i = 0; i < 10; i++) rpCoarse.step (); 
+    		System.out.println ("t=" + second + "s: \t" + 
+				nf.format (Math.toDegrees (sp.getTheta (second))) 
+				+ "\t" +
+				nf.format (Math.toDegrees (rp.getLastTheta ()))
+				+ "\t" + 
+				nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
+    	}
+
     }
 }
 
